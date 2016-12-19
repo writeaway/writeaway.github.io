148b9ff15e0304a0355e35c23425624106e3a15d
AndrewK <andrew@spiralscout.com>
2016-12-19 18:05:59 +0300

    Code consistency

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ var Redaxtor = require('../../redaxtor/src/index');
 var RedaxtorMedium = require('../../redaxtor-medium/src/index');
 var RedaxtorCodemirror = require('../../redaxtor-codemirror/src/index');
 
+require('../../redaxtor/dist/redaxtor.css');
 require('../../redaxtor-medium/dist/medium-editor.css');
 require('../../redaxtor-medium/dist/redaxtor-medium.css');
+
 require('../node_modules/codemirror/lib/codemirror.css');
 
 var components = {
